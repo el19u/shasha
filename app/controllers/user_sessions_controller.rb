504afee5f73ab57dcb7471a13a9de6b5952e5b65
yen <akainooo@gmail.com>
2022-10-18 00:51:19 +0800

    fix: login error

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,13 +3,13 @@ class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
   def create
-    @user = login(params[:email], params[:password])
+    @user = login(params[:user][:email], params[:user][:password])
 
     if @user
       redirect_back_or_to(root_path, notice: "Logged in successfully")
     else
       flash.now[:alert] = "Login failed"
-      render "new"
+      render("new", status: :unprocessable_entity)
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "User created successfully"
+      redirect_to(root_path, notice: "User created successfully")
     else
-      render :new, status: :unprocessable_entity
+      render("new", status: :unprocessable_entity)
     end
   end
 
